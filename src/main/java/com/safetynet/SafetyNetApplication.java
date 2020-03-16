c17f3a82900a43ea07100b46a4207d557381061d
bende47 <bendejustin@gmail.com>
2020-03-16 20:10:05 +0100

    Execution du projet

--- src/main/java/com/safetynet/SafetyNetApplication.java
+++ src/main/java/com/safetynet/SafetyNetApplication.java
@@ -2,12 +2,25 @@ package com.safetynet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.safetynet.metier.ReadJson;
+import com.safetynet.model.Safetynet;
 
 @SpringBootApplication
 public class SafetyNetApplication {
 
+	private static String file = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json";
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetApplication.class, args);
 	}
+	
+	@Bean
+	public Safetynet loadModel()  {
+		return ReadJson.readJsonFile(file);
+	}
+	
+	
 
 }
