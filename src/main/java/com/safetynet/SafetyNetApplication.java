84e81440df358ab1dfc168fe718368034f368909
bende47 <bendejustin@gmail.com>
2020-03-21 12:13:44 +0100

    Execution du programmme

--- src/main/java/com/safetynet/SafetyNetApplication.java
+++ src/main/java/com/safetynet/SafetyNetApplication.java
@@ -11,7 +11,8 @@ import com.safetynet.model.Safetynet;
 public class SafetyNetApplication {
 
 	private static String file = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json";
-
+	//private static String file = "./src/main/resources/data.json";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetApplication.class, args);
 	}
