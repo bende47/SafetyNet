e68ff7cf21ea378d266a43ed8e41aff1212b16d1
bende47 <bendejustin@gmail.com>
2020-04-04 23:54:51 +0200

    PersonController pour les endpoints en relation avec person

--- src/main/java/com/safetynet/controller/PersonController.java
+++ src/main/java/com/safetynet/controller/PersonController.java
@@ -1,10 +1,6 @@
 package com.safetynet.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
