50d07af218d5dc4f4d970d8d4a3a62b263f8d663
bende47 <bendejustin@gmail.com>
2020-04-04 23:51:45 +0200

    Lecture du fichier Json et lancement de l'application

--- src/main/java/com/safetynet/SafetyNetApplication.java
+++ src/main/java/com/safetynet/SafetyNetApplication.java
@@ -1,5 +1,8 @@
 package com.safetynet;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,16 +12,17 @@ import com.safetynet.model.Safetynet;
 
 @SpringBootApplication
 public class SafetyNetApplication {
+	@Autowired
+	private ReadJson readJson;
 
-	private static String file = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json";
-	//private static String file = "./src/main/resources/data.json";
+	private  String file = "./src/main/resources/data.json";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetApplication.class, args);
 	}
 	
 	@Bean
-	public Safetynet loadModel()  {
-		return ReadJson.readJsonFile(file);
+	public Safetynet loadModel() throws IOException  {
+		return readJson.readFile(file);
 	}
 }
