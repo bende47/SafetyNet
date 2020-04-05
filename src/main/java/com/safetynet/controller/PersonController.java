5dafe5ee33cbd6e0d2522de2ea94c9ddbdc2abb7
bende47 <bendejustin@gmail.com>
2020-04-05 18:59:15 +0200

    Controller Person pour traiter tous les endpoints concernant Person

--- src/main/java/com/safetynet/controller/PersonController.java
+++ src/main/java/com/safetynet/controller/PersonController.java
@@ -1,16 +1,22 @@
 package com.safetynet.controller;
 
+import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.safetynet.metier.PersonMetier;
+import com.safetynet.model.Firestation;
 import com.safetynet.model.Person;
+import com.safetynet.model.PersonInfo;
 import com.safetynet.model.Safetynet;
 
 
@@ -42,6 +48,25 @@ public class PersonController {
 		listPerson = safetynet.getPersons();		
 		listPerson=personMetier.deletePersonne(person);
 		return safetynet;
+	}
+	
+	@GetMapping(value = "/phoneAlert")
+	public List<String> phoneAlert (@RequestParam String firestation) {			
+		Map<Integer, Firestation> listFirestation = safetynet.getFirestations();
+		listPerson = safetynet.getPersons();
+		return personMetier.phoneAlert(firestation, listFirestation, listPerson);
+	}
+	
+	@GetMapping(value = "/personInfo")
+	public List<PersonInfo> personInfo (@RequestParam String firstName, String lastName) throws ParseException {			
+		listPerson = safetynet.getPersons();
+		return personMetier.listPersonInfo(firstName, lastName, listPerson);
+	}
+	
+	@GetMapping(value = "/communityEmail")
+	public List<String> communityEmail(@RequestParam String city) {
+		listPerson = safetynet.getPersons();
+		return personMetier.communityEmail(city, listPerson);
 	}
 	
 	
