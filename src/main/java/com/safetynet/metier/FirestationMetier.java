a6d355e52cdc6f33059925faf8b9c2385dcf3e9d
bende47 <bendejustin@gmail.com>
2020-04-05 19:00:23 +0200

    Service Firestation contient les differents services

--- src/main/java/com/safetynet/metier/FirestationMetier.java
+++ src/main/java/com/safetynet/metier/FirestationMetier.java
@@ -1,9 +1,14 @@
 package com.safetynet.metier;
 
 
+import java.text.ParseException;
+import java.util.List;
 import java.util.Map;
 
 import com.safetynet.model.Firestation;
+import com.safetynet.model.Person;
+import com.safetynet.model.PersonHabitant;
+import com.safetynet.model.PersonStation;
 import com.safetynet.model.Safetynet;
 
 public interface FirestationMetier {
@@ -12,6 +17,12 @@ public interface FirestationMetier {
 	public Map<Integer, Firestation> updateFirestation(int stationNumber, String stationAdress,
 			Map<Integer, Firestation> getfirestations);
 	public Map<Integer, Firestation> deleteFirestation(int stationNumber, String stationAdress,
-			Map<Integer, Firestation> getfirestations);	
+			Map<Integer, Firestation> firestations);	
+	public  PersonStation stationNumber(String stationNumber,
+			Map<Integer, Firestation> firestations,List<Person> listPerson) throws ParseException;
+	public  List<PersonHabitant> fire(String address, List<Person> listPerson,
+			Map<Integer, Firestation> firestations) throws ParseException;
+	public  List<Person> flood(String stations, Map<Integer, Firestation> firestations,
+			Map<String, List<Person>> personFoyers) throws ParseException ;
 	
 }
--- src/main/java/com/safetynet/metier/FirestationMetierImpl.java
+++ src/main/java/com/safetynet/metier/FirestationMetierImpl.java
@@ -1,6 +1,10 @@
 package com.safetynet.metier;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.safetynet.model.Firestation;
+import com.safetynet.model.Person;
+import com.safetynet.model.PersonHabitant;
+import com.safetynet.model.PersonStation;
 import com.safetynet.model.Safetynet;
 
 @Service
@@ -19,6 +26,9 @@ public class FirestationMetierImpl implements FirestationMetier {
 
 	@Autowired
 	private Safetynet safetynet;
+	
+	@Autowired
+	private PersonMetier personMetier;
 
 	@Override
 	public Safetynet addFirestation(Map<?, ?> f) {		
@@ -66,6 +76,101 @@ public class FirestationMetierImpl implements FirestationMetier {
 			}
 		}
 		return getfirestations;
+	}
+
+	@Override
+	public PersonStation stationNumber(String stationNumber, Map<Integer, Firestation> firestations,
+			List<Person> listPerson) throws ParseException {
+		List<Person> listPersonInfo = new ArrayList<>();
+		Set<String> addresses = new HashSet<>();
+
+		Set cles = firestations.keySet();
+		Iterator it = cles.iterator();
+
+		while (it.hasNext()) {
+			Object station = it.next();
+			Firestation fire = firestations.get(Integer.parseInt(stationNumber));
+
+			if (fire.getStation() == Integer.parseInt(stationNumber)) {
+				addresses = fire.getAddresses();
+			}
+		}
+
+		for (Person person : listPerson) {
+			if (addresses.contains(person.getAddress())) {
+				listPersonInfo.add(person);
+			}
+		}
+
+		int adulte = 0;
+		for (Person person : listPersonInfo) {
+			if (personMetier.personAdulte(person.getMedicalRecord().getBirthdate())) {
+				adulte++;
+			}
+		}
+		int enfant = listPersonInfo.size() - adulte;
+		return new PersonStation(listPersonInfo, adulte, enfant);
+	}
+
+	@Override
+	public List<PersonHabitant> fire(String address, List<Person> listPerson, Map<Integer, Firestation> firestations)
+			throws ParseException {
+		List<PersonHabitant> listPersonHabitant = new ArrayList<>();
+
+		Set cles = firestations.keySet();
+		Iterator it = cles.iterator();
+
+		while (it.hasNext()) {
+			Object station = it.next();
+			Firestation fire = firestations.get(station);
+			if (fire.getAddresses().contains(address)) {
+				int stationNum = fire.getStation();
+				for (Person person : listPerson) {
+					if (person.getAddress().equals(address)) {
+						int age = personMetier.determineAge(person.getMedicalRecord().getBirthdate());
+						PersonHabitant persHabitant = new PersonHabitant(age, person, stationNum);
+						listPersonHabitant.add(persHabitant);
+					}
+				}
+			}
+		}
+
+		return listPersonHabitant;
+	}
+
+	@Override
+	public List<Person> flood(String stations, Map<Integer, Firestation> firestations, Map<String, List<Person>> Foyers)
+			throws ParseException {
+
+		List<Person> listPersonInfo = new ArrayList<Person>();
+		Set<String> addresses = new HashSet<String>();
+
+		Set cles = firestations.keySet();
+		Iterator it = cles.iterator();
+
+		while (it.hasNext()) {
+			Object station = it.next();
+			Firestation fire = firestations.get(Integer.parseInt(stations));
+
+			if (fire.getStation() == Integer.parseInt(stations)) {
+				addresses = fire.getAddresses();
+			}
+		}
+
+		Set key = Foyers.keySet();
+		Iterator iter = key.iterator();
+		while (iter.hasNext()) {
+			Object num = iter.next();
+			List<Person> persons = Foyers.get(num);
+
+			for (Person person : persons) {
+				if (addresses.contains(person.getAddress())) {
+					listPersonInfo.add(person);
+				}
+			}
+		}
+
+		return listPersonInfo;
 	}
 
 }
